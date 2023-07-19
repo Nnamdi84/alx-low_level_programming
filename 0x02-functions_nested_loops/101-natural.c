b1b985e3a1689fe25ed46313f6a46e08d332b872
Nnamdi84 <favour.uche.84@gmail.com>
2023-07-18 21:50:09 -0700

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,3 @@
-/**
- * File: 101-natural.c
- * Auth: vincent kip
- */
-
-
 #include <stdio.h>
 
 
@@ -17,7 +11,8 @@
 int main(void)
 	{
 		int i, sum = 0;
-	
+
+
 		for (i = 0; i < 1024; i++)
 		{
 			if ((i % 3) == 0 || (i % 5) == 0)
